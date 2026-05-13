500018f6a32b9bbbf05ada3b9ff540aa05317b9b
artifact911 <artifact911@gmail.com>
2026-05-13 23:34:32 +0300

    init commit

---
+++ src/main/java/com/art/springsecurityrunner/SpringSecurityRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.art.springsecurityrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityRunnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/art/springsecurityrunner/SpringSecurityRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.springsecurityrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
